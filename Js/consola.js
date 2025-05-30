cfa859c093bd4f901a7fd02b7d3ef37bbdfe8fa8
jesusito personal <hectorjesusm08@gmail.con>
2025-05-29 23:28:40 -0600

    comentar y estudiar

---
+++ Js/consola.js
@@ -0,0 +1,8 @@
+function mostrarMensajes(){
+    console.log("Este es un mensaje para la consola de JS");
+    console.log("Suma: ", 10 + 15);
+    console.info("Mostrando mensaje de informacion desde consola js");
+    console.warn("Mostrando un mensaje de enmergencia de JS");
+    console.error("Mostrando mensaje de error desde consola de js");
+
+}
